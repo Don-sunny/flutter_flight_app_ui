edecc99ee49cfcacd26b98ecdd55b5c0fe48176f
Don-sunny <donsunny544@gmail.com>
2023-12-18 11:39:32 +0530

    -

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,28 @@ import 'package:flight_app_ui/screens/flightBooking/add_flight.dart';
 import 'package:flight_app_ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
   runApp(MyApp());
-
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           useMaterial3: true,
-        primaryColor:const  Color(0xff415a5c),
-        indicatorColor:const  Color(0xffffcfa1),
-        canvasColor:const  Color(0xff9dafb1)
-        //canvasColor: const Color(0xff597672),
+          primaryColor: const Color(0xff415a5c),
+          indicatorColor: const Color(0xffffcfa1),
+          canvasColor: const Color(0xff9dafb1)
+          //canvasColor: const Color(0xff597672),
 
-      ),
+          ),
+
       /// SPLASH SCREEN
-      home: SplashScreen(),
-
+      home: const SplashScreen(),
     );
   }
 }
